074b97068601a11251ed3321b1fb3dfb565f249c
staceygrasso <59070489+staceygrasso@users.noreply.github.com>
2020-02-26 14:39:15 -0500

    Add files via upload

---
+++ Project5/src/Die.java
@@ -0,0 +1,46 @@
+import java.util.Random;
+
+ public class Die {
+
+    private int face;  //value showing on the face of the die
+
+    private Random rand = new Random(); //random object to use for rolling
+
+    //constructor whcih initializes the die to show a 3 
+    public Die() {
+
+    }
+
+    //rolls the die and updates it's face
+    public void roll() {
+
+
+    }
+
+    
+    //returns the value of the die face
+    public int getFace() {
+
+
+    }
+
+
+
+    //tests the Die class
+    public static void main (String [] args) {
+
+    /* Add code here that:
+        *  creates one Die object
+        *  rolls the Die 5 times
+        *  prints out the total of the rolls
+        *  prints out the actual average value of all rolls
+    */
+
+         
+   
+
+
+
+    }
+
+ }
